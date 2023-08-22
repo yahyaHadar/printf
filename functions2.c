ebd455ba121d511e660d4c4201fc4c5e4bb8de86
josephnady <jozephnady@gmail.com>
2023-08-22 16:37:09 -0700

    Update is_printable 1

--- functions2.c
+++ functions2.c
@@ -86,7 +86,7 @@ int print_non_printable(va_list types, char buffer[],
 
 	while (str[i] != '\0')
 	{
-		if (is_printable(str[i]))
+		if (new_is_printable(str[i]))
 			buffer[i + offset] = str[i];
 		else
 			offset += append_hexa_code(str[i], buffer, i + offset);
